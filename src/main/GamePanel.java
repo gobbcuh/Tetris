85cf23b54aef40fdf3390d3d5cf660bf96efb4dd
gobbcuh <jajalorino@gmail.com>
2025-04-05 20:45:54 +0800

    activated thread

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,14 +5,27 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     public static final int WIDTH = 1200;
     public static final int HEIGHT = 720;
+    final int FPS = 60; // Frames per second
+    Thread gameThread; // Thread for the game loop
 
     public GamePanel() {
         // Panel Settings
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.BLACK);
         this.setLayout(null); // No layout manager, absolute positioning
+    }
+
+    public void launchGame() {
+        gameThread = new Thread(this); // Create a new thread for the game loop
+        gameThread.start(); // Start the game loop
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
     }
 }
