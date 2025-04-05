2edcf90d0feaab39cdb64bd89642e552b6ed4853
gobbcuh <jajalorino@gmail.com>
2025-04-05 20:56:00 +0800

    added main play area

---
+++ src/main/PlayManager.java
@@ -0,0 +1,12 @@
+package main;
+
+public class PlayManager {
+
+    // Main Play Area
+    final int WIDTH = 360;
+    final int HEIGHT = 600;
+    public static int left_x;
+    public static int right_x;
+    public static int top_y;
+    public static int bottom_y;
+}
