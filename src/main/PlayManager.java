d5f90664eae564d8bfaed8bf6894febcb5bcd6e5
gobbcuh <jajalorino@gmail.com>
2025-04-05 21:23:31 +0800

    updated again

--- src/main/PlayManager.java
+++ src/main/PlayManager.java
@@ -2,7 +2,9 @@ package main;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 public class PlayManager {
 
@@ -38,6 +40,9 @@ public class PlayManager {
         int x = right_x + 100;
         int y = bottom_y - 200;
         g2.drawRect(x, y, 200, 200);
+        g2.setFont(new Font("Arial", Font.PLAIN, 30));
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2.drawString("NEXT", x + 60, y + 60);
 
     }
 }
