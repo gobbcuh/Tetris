51b7bd7fff83751c0e8beaab0ff1626a0c74d76a
gobbcuh <jajalorino@gmail.com>
2025-04-08 23:42:13 +0800

    added 2 other classes

--- src/mino/Block.java
+++ src/mino/Block.java
@@ -1,3 +1,20 @@
-public class Block {
-    
+package mino;
+
+import java.awt.Rectangle;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class Block extends Rectangle {
+    public int x, y;
+    public static final int SIZE = 30; // 30x30 block
+    public Color c;
+
+    public Block(Color c) {
+        this.c = c;
+    }
+
+    public void draw(Graphics2D g2) {
+        g2.setColor(c);
+        g2.fillRect(x, y, SIZE, SIZE);
+    }
 }
