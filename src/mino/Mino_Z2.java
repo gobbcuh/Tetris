5d09bcf7a9d8c9c9cd51b7cea7680c7b94fda507
jasmin <jajalorino@gmail.com>
2025-07-23 12:53:07 +0800

    Z2 constructor initialized

--- src/mino/Mino_Z2.java
+++ src/mino/Mino_Z2.java
@@ -1,5 +1,22 @@
 package mino;
 
-public class Mino_Z2 {
-    
+import java.awt.*;
+
+public class Mino_Z2 extends Mino {
+
+    public Mino_Z2() {
+        create(Color.green);
+    }
+
+    public void setXY(int x, int y) {
+
+        b[0].x = x;
+        b[0].y = y;
+        b[1].x = b[0].x;
+        b[1].y = b[0].y - Block.SIZE;
+        b[2].x = b[0].x + Block.SIZE;
+        b[2].y = b[0].y;
+        b[3].x = b[0].x + Block.SIZE;
+        b[3].y = b[0].y + Block.SIZE;
+    }
 }
