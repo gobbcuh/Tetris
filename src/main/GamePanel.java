26922146e03853a8a5cd34fe75f7ce74d19c353a
jasmin <jajalorino@gmail.com>
2025-07-23 15:39:28 +0800

    added game sound

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -13,6 +13,8 @@ public class GamePanel extends JPanel implements Runnable {
     final int FPS = 60;
     Thread gameThread;
     PlayManager pm;
+    public static Sound music = new Sound();
+    public static Sound se = new Sound();
 
     public GamePanel() {
         // panel settings
@@ -30,6 +32,9 @@ public class GamePanel extends JPanel implements Runnable {
     public void launchGame() {
         gameThread = new Thread(this);
         gameThread.start();
+
+        music.play(0, true);
+        music.loop();
     }
 
     @Override
