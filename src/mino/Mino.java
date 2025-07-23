8511f661ccbdbe0f62561f086c7c9a7a8570d068
jasmin <jajalorino@gmail.com>
2025-07-23 09:35:11 +0800

    finished all directions

--- src/mino/Mino.java
+++ src/mino/Mino.java
@@ -24,8 +24,20 @@ public class Mino {
     }
 
     public void setXY(int x, int y) {}
-    public void updateXY(int direction) {}
-    public void getDirection() {}
+    public void updateXY(int direction) {
+
+        this.direction = direction;
+        b[0].x = tempB[0].x;
+        b[0].y = tempB[0].y;
+        b[1].x = tempB[1].x;
+        b[1].y = tempB[1].y;
+        b[2].x = tempB[2].x;
+        b[2].y = tempB[2].y;
+        b[3].x = tempB[3].x;
+        b[3].y = tempB[3].y;
+    }
+
+    public void getDirection1() {}
     public void getDirection2() {}
     public void getDirection3() {}
     public void getDirection4() {}
